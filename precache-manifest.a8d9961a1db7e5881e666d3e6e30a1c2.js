ca90983afb6a5e6f0b3b1b3bbbf711278d0c7f20
sansil <sansil32@gmail.com>
2019-05-09 23:49:27 -0300

    deploy

---
+++ precache-manifest.a8d9961a1db7e5881e666d3e6e30a1c2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c39f4c5bc9cc3e71805f9160ca500dbd",
+    "url": "/busqueda-cumple-sofi/img/textura.c39f4c5b.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/busqueda-cumple-sofi/robots.txt"
+  },
+  {
+    "revision": "77bb59852691ee0f177db605e4efb9c0",
+    "url": "/busqueda-cumple-sofi/img/naranja_pista.77bb5985.jpeg"
+  },
+  {
+    "revision": "d8dee95477851a9b135d",
+    "url": "/busqueda-cumple-sofi/css/app.554275dc.css"
+  },
+  {
+    "revision": "d7347102b8967ba3f334",
+    "url": "/busqueda-cumple-sofi/js/chunk-vendors.71737de2.js"
+  },
+  {
+    "revision": "aa50a063c416d9a44696994689c984fa",
+    "url": "/busqueda-cumple-sofi/img/map3.aa50a063.png"
+  },
+  {
+    "revision": "c27764e559f76fc426a845da48904988",
+    "url": "/busqueda-cumple-sofi/img/hojaVid.c27764e5.jpeg"
+  },
+  {
+    "revision": "da312025547ba9d2d3b31981ad1c95de",
+    "url": "/busqueda-cumple-sofi/img/codigo_diccionario.da312025.png"
+  },
+  {
+    "revision": "b458fe399cb1a5262e1b",
+    "url": "/busqueda-cumple-sofi/js/about.0364b854.js"
+  },
+  {
+    "revision": "d8dee95477851a9b135d",
+    "url": "/busqueda-cumple-sofi/js/app.3f7164a1.js"
+  },
+  {
+    "revision": "57bcd0d9fec0a2b8ede2fe787c43eed2",
+    "url": "/busqueda-cumple-sofi/img/mapa.57bcd0d9.jpeg"
+  },
+  {
+    "revision": "76b464c21d03f5ad1519488100f9fe49",
+    "url": "/busqueda-cumple-sofi/img/ingenio.76b464c2.jpg"
+  },
+  {
+    "revision": "fdd913ff42299da0834bfcb6e362c5fb",
+    "url": "/busqueda-cumple-sofi/img/test2.fdd913ff.png"
+  },
+  {
+    "revision": "74e965f448cc48dcc6384681666563b0",
+    "url": "/busqueda-cumple-sofi/img/sause_pista.74e965f4.jpeg"
+  },
+  {
+    "revision": "850c3f8a735c24af42a309896bed1bee",
+    "url": "/busqueda-cumple-sofi/img/Preboda-36.850c3f8a.jpg"
+  },
+  {
+    "revision": "e173fc50175798efb84e53ccdde1c388",
+    "url": "/busqueda-cumple-sofi/index.html"
+  },
+  {
+    "revision": "b458fe399cb1a5262e1b",
+    "url": "/busqueda-cumple-sofi/css/about.dbc38d35.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/busqueda-cumple-sofi/precache-manifest.a8d9961a1db7e5881e666d3e6e30a1c2.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "cumple-sofi"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
